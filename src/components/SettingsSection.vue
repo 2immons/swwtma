886af76500de94c4849d1afa49f0b2c249230d20
Pavel <podoprigoraps@gmail.com>
2024-09-23 23:47:07 +0400

    Сделан раздел настроек

--- src/components/SettingsSection.vue
+++ src/components/SettingsSection.vue
@@ -1,10 +1,121 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SliderButton from "@/components/SliderButton.vue";
+import { onBeforeUnmount, onMounted, ref } from "vue";
+
+const isStockExchangeMenuVisible = ref(false);
+const isConfirmModalVisible = ref(false);
+
+const toggleConfirmModal = () => {
+  isConfirmModalVisible.value = !isConfirmModalVisible.value;
+};
+
+const toggleStockExchangeMenu = () => {
+  isStockExchangeMenuVisible.value = !isStockExchangeMenuVisible.value;
+};
+
+const handleClickOutside = (event: any) => {
+  const languageSettingsElement = document.querySelector(
+    ".stock-exchange-menu"
+  );
+  const languageWrapperElement = document.querySelector(
+    ".stock-exchange-wrapper"
+  );
+
+  const accountDelete = document.querySelector(".confirm-modal");
+  const accountDeleteWrapper = document.querySelector(".account-wrapper");
+
+  if (
+    isConfirmModalVisible.value &&
+    accountDelete &&
+    accountDeleteWrapper &&
+    !accountDelete.contains(event.target) &&
+    !accountDeleteWrapper.contains(event.target)
+  ) {
+    isConfirmModalVisible.value = false;
+  }
+
+  if (
+    isStockExchangeMenuVisible.value &&
+    languageSettingsElement &&
+    languageWrapperElement &&
+    !languageSettingsElement.contains(event.target) &&
+    !languageWrapperElement.contains(event.target)
+  ) {
+    isStockExchangeMenuVisible.value = false;
+  }
+};
+
+onMounted(async () => {
+  document.addEventListener("click", handleClickOutside);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener("click", handleClickOutside);
+});
+
+const setStock = (stock: string) => {
+  console.log(stock);
+};
+</script>
 
 <template>
   <section class="settings">
     <div class="container">
       <div class="settings-content">
         <h2>Settings</h2>
+        <div class="settings-list">
+          <div
+            class="settings-wrapper stock-exchange-wrapper"
+            @click="toggleStockExchangeMenu"
+          >
+            <h3>Сменить биржу</h3>
+            <p>Bybit</p>
+            <img src="../assets/svg/nav/arrow-right.svg" alt="" />
+            <div class="stock-exchange-menu" v-if="isStockExchangeMenuVisible">
+              <button @click="setStock('Bybit')" class="language-btn">
+                <img src="../assets/svg/flags/eng.svg" alt="English language" />
+                Bybit
+              </button>
+              <button @click="setStock('ru')" class="language-btn">
+                <img src="../assets/svg/flags/rus.svg" alt="Russian language" />
+                Test
+              </button>
+            </div>
+          </div>
+          <div class="settings-wrapper referal-wrapper">
+            <h3>Стать рефералом</h3>
+            <p></p>
+          </div>
+          <div
+            class="settings-wrapper account-wrapper"
+            @click="toggleConfirmModal"
+          >
+            <h3>Удалить аккаунт</h3>
+            <img src="../assets/svg/nav/arrow-right.svg" alt="" />
+            <div class="confirm-modal" v-if="isConfirmModalVisible">
+              <h3>Вы уверены?</h3>
+              <div class="confirm-modal__buttons">
+                <button class="confirm-btn" @click="deleteAccount">
+                  Да, удалить
+                </button>
+                <button class="deny-btn" @click="toggleConfirmModal">
+                  Нет, отменить
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="sliders">
+            <div class="slider-setting">
+              <h3>Вибрация</h3>
+              <SliderButton />
+            </div>
+            <div class="slider-setting">
+              <h3>Анимация монет</h3>
+              <SliderButton />
+            </div>
+          </div>
+        </div>
+        <p class="politic">Политика конфедициальности</p>
       </div>
     </div>
   </section>
@@ -22,5 +133,103 @@
   flex-direction: column
   display: flex
 h2
+  color: $c-light-text
+
+.settings-list
+  margin-top: 10px
+  width: 100%
+  display: flex
+  flex-direction: column
+  gap: 8px
+
+.settings-wrapper
+  background: $c-element-bg
+  box-shadow: $c-element-shadow
+  border-radius: 12px
+  display: flex
+  flex-direction: column
+  height: 60px
+  align-items: start
+  justify-content: center
+  padding: 0 15px
+  position: relative
+
+  img
+    position: absolute
+    right: 15px
+
+.stock-exchange-wrapper, .account-wrapper
+  position: relative
+
+.confirm-modal
+  width: 100%
+  background: $c-active-element
+  position: absolute
+  z-index: 2
+  top: 63px
+  left: 0
+  display: flex
+  flex-direction: column
+  align-items: start
+  padding: 10px
+  gap: 10px
+  border-radius: 10px
+
+  h3
+    width: 100%
+    text-align: center
+  .confirm-modal__buttons
+    display: flex
+    width: 100%
+    justify-content: space-around
+
+    button
+      background: $c-active-element
+      color: $c-main-text
+      font-size: 18px
+      border-radius: 12px
+      border: 1px solid $c-main-text
+      padding: 5px 10px
+      box-shadow: $c-element-shadow
+
+.stock-exchange-menu
+  height: 100px
+  width: 100%
+  background: $c-active-element
+  position: absolute
+  z-index: 2
+  top: 63px
+  left: 0
+  display: flex
+  flex-direction: column
+  align-items: start
+  padding: 10px
+  gap: 10px
+  border-radius: 10px
+
+  button
+    display: flex
+    align-items: center
+    gap: 10px
+    font-size: 20px
+    color: $c-main-text
+
+.sliders
+  margin-top: 10px
+  width: 100%
+  display: flex
+  flex-direction: column
+  gap: 10px
+  color: $c-light-text
+
+.slider-setting
+  display: flex
+  justify-content: space-between
+  padding: 0 0 0 15px
+  align-items: center
+
+
+.politic
+  margin-top: 30px
   color: $c-light-text
 </style>
---
+++ src/components/SliderButton.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const sliderClass = ref("left");
+const sliderStatus = ref(false);
+const moveSlider = () => {
+  if (sliderClass.value === "left") {
+    sliderClass.value = "right";
+    sliderStatus.value = true;
+  } else {
+    sliderClass.value = "left";
+    sliderStatus.value = false;
+  }
+};
+</script>
+
+<template>
+  <div class="slider-wrapper" @click="moveSlider">
+    <button :class="sliderClass"></button>
+  </div>
+</template>
+
+<style scoped lang="sass">
+@import "src/styles/variables"
+.slider-wrapper
+  position: relative
+  width: 80px
+  height: 35px
+  border-radius: 21px
+  background: $c-element-bg
+  box-shadow: $c-element-shadow
+
+  .left, .right
+    position: absolute
+    height: 25px
+    border-radius: 21px
+    background: $c-main-text
+    width: 25px
+    top: 50%
+    transform: translateY(-50%)
+    left: 7px
+    transition: ease 0.2s
+  .right
+    left: 48px
+    transition: ease 0.2s
+</style>
