a17f3ab25f27bdf151778ea477287e0b0a648171
2immons <podoprigoraps@gmail.com>
2024-09-26 21:54:56 +0400

    Updates

---
+++ js/568.dd9606db.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksave_world_web_tg_app"]=self["webpackChunksave_world_web_tg_app"]||[]).push([[568],{3919:function(e,n,u){u.r(n),u.d(n,{default:function(){return _}});var a=u(6768),l=u(3256),r=u(8449),t=(0,a.pM)({__name:"FriendsView",setup(e){return(e,n)=>((0,a.uX)(),(0,a.CE)(a.FK,null,[(0,a.bF)(l.A),n[0]||(n[0]=(0,a.Lk)("main",null,null,-1)),(0,a.bF)(r.A)],64))}});const s=t;var _=s}}]);
+//# sourceMappingURL=568.dd9606db.js.map
