1b93ec9e0b8eebb436a7850a74dab834e8287b62
Pavel <podoprigoraps@gmail.com>
2024-10-19 01:54:43 +0400

    Updates

--- js/365.06f2eea1.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunksave_world_web_tg_app"]=self["webpackChunksave_world_web_tg_app"]||[]).push([[365],{4997:function(e,n,u){u.r(n),u.d(n,{default:function(){return p}});var a=u(6768),l=u(7005),t=u(794),r=(0,a.pM)({__name:"BalanceView",setup(e){return(e,n)=>((0,a.uX)(),(0,a.CE)(a.FK,null,[(0,a.bF)(l.A),n[0]||(n[0]=(0,a.Lk)("main",null,null,-1)),(0,a.bF)(t.A)],64))}});const _=r;var p=_}}]);
-//# sourceMappingURL=365.06f2eea1.js.map
