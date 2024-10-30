cda72e47c45173c4d7e28d92567e9b700128261e
2immons <podoprigoraps@gmail.com>
2024-10-30 11:46:35 +0400

    Updates

---
+++ js/888.bc9bd550.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksave_world_web_tg_app"]=self["webpackChunksave_world_web_tg_app"]||[]).push([[888],{4997:function(e,n,u){u.r(n),u.d(n,{default:function(){return p}});var a=u(6768),l=u(6307),t=u(794),r=(0,a.pM)({__name:"BalanceView",setup(e){return(e,n)=>((0,a.uX)(),(0,a.CE)(a.FK,null,[(0,a.bF)(l.A),n[0]||(n[0]=(0,a.Lk)("main",null,null,-1)),(0,a.bF)(t.A)],64))}});const _=r;var p=_}}]);
+//# sourceMappingURL=888.bc9bd550.js.map
