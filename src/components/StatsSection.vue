4874025baa6fa0dc7059f2ec493bbbcf3e0903f4
Pavel <podoprigoraps@gmail.com>
2025-02-23 23:52:55 +0400

    ВСЕ

--- src/components/StatsSection.vue
+++ src/components/StatsSection.vue
@@ -264,7 +264,7 @@ const isProd = import.meta.env.MODE === "production";
 
 .interface--center
   margin: 0 !important
-  padding-right: 0
+  padding-right: 0 !important
 
   .claim-btn
     display: flex
